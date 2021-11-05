97bae100a3e388cf90adb29a65db3449e3e4f49a
haytham10 <cnarabi@gmail.com>
2021-11-05 18:25:13 +0100

    18250511

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hmokhtar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 20:25:45 by hmokhtar          #+#    #+#             */
-/*   Updated: 2021/11/05 16:18:46 by hmokhtar         ###   ########.fr       */
+/*   Updated: 2021/11/05 18:24:46 by hmokhtar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-int	ft_atoi(char	*str);
+int		ft_atoi(char	*str);
 void	*ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
-int	ft_isalnum(char	*str);
-int	ft_isalpha(char	*str);
-int	ft_isascii(char	*str);
-int	ft_isdigit(char	*str);
-int	ft_isprint(char	*str);
+int		ft_isalnum(char	*str);
+int		ft_isalpha(char	*str);
+int		ft_isascii(char	*str);
+int		ft_isdigit(char	*str);
+int		ft_isprint(char	*str);
 void	*ft_memccpy(void *restrict dest, const void *restrict src,
-		int c, size_t n);
+			int c, size_t n);
 void	*ft_memchr(const void *str, int c, size_t n);
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *ptr, int x, size_t n);
@@ -34,8 +34,8 @@ char	*ft_strchr(char	*str, int c);
 char	*ft_strdup(char *s1);
 size_t	ft_strlcat(char	*dest, char	*src, size_t n);
 size_t	ft_strlcpy(char	*dest, char	*src, size_t n);
-int	ft_strlen(char	*str);
-int	ft_strncmp(const char	*str1, const char	*str2, size_t	n);
+int		ft_strlen(char	*str);
+int		ft_strncmp(const char	*str1, const char	*str2, size_t	n);
 char	*ft_strnstr(char	*s1, char	*s2, size_t	n);
 char	*ft_strrchr(const char *str, int c);
 char	*ft_tolower(char	*str);
