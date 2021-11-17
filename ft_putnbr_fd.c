780996aab61490a5c8c2a37f1719a596df4fe774
haytham10 <cnarabi@gmail.com>
2021-11-17 01:12:17 +0100

    01121611

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,13 +6,13 @@
 /*   By: hmokhtar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 00:04:51 by hmokhtar          #+#    #+#             */
-/*   Updated: 2021/11/14 21:30:43 by hmokhtar         ###   ########.fr       */
+/*   Updated: 2021/11/17 01:12:07 by hmokhtar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static unsigned int	ft_module(int n)
+static unsigned int	ft_va(int n)
 {
 	if (n < 0)
 		return (n * (-1));
@@ -26,7 +26,7 @@ void	ft_putnbr_fd(int n, int fd)
 
 	if (n < 0)
 		ft_putchar_fd('-', fd);
-	num = ft_module(n);
+	num = ft_va(n);
 	if (num >= 10)
 	{
 		ft_putnbr_fd(num / 10, fd);
