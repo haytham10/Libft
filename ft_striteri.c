bdf19b57f4486a063adb6a8ad4786d71d2fb7cf3
haytham10 <cnarabi@gmail.com>
2021-11-17 22:45:52 +0100

    22451711

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: hmokhtar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 00:29:09 by hmokhtar          #+#    #+#             */
-/*   Updated: 2021/11/14 21:32:42 by hmokhtar         ###   ########.fr       */
+/*   Updated: 2021/11/17 22:44:48 by hmokhtar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 	int	i;
 	int	j;
 
-	i = 0;
-	j = 0;
 	if (!s && !f)
 		return ;
 	i = ft_strlen(s);
+	j = 0;
 	while (j < i)
 	{
 		(*f)(j, s);
