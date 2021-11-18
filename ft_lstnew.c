ef869da0ee1065c9851c7117a510734853b3416e
haytham10 <cnarabi@gmail.com>
2021-11-18 17:49:07 +0100

    17491811

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -1,37 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hmokhtar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/13 00:04:51 by hmokhtar          #+#    #+#             */
-/*   Updated: 2021/11/17 01:12:07 by hmokhtar         ###   ########.fr       */
+/*   Created: 2021/11/18 17:23:23 by hmokhtar          #+#    #+#             */
+/*   Updated: 2021/11/18 17:23:23 by hmokhtar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static unsigned int	ft_va(int n)
+t_list  *ft_lstnew(void *content)
 {
-	if (n < 0)
-		return (n * (-1));
-	else
-		return (n);
-}
+    t_list  *head;
 
-void	ft_putnbr_fd(int n, int fd)
-{
-	unsigned int	num;
-
-	if (n < 0)
-		ft_putchar_fd('-', fd);
-	num = ft_va(n);
-	if (num >= 10)
-	{
-		ft_putnbr_fd(num / 10, fd);
-		ft_putnbr_fd(num % 10, fd);
-	}
-	else
-		ft_putchar_fd(num + '0', fd);
+    head = malloc(sizeof(t_list));
+    head->content = content;
+    return (content);
 }
--- libft.h
+++ libft.h
@@ -16,11 +16,13 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-/*typedef struct s_list
+typedef struct s_list
 {
     void *content;
     struct s_list *next;
-} t_list;*/
+} t_list;
+
+t_list  *ft_lstnew(void *content);
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
