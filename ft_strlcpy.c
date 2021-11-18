c5b770fda5fcc2092ba9c4ca6e81f55af12c20e0
haytham10 <cnarabi@gmail.com>
2021-11-18 04:18:41 +0100

    04181811

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: hmokhtar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 01:44:42 by hmokhtar          #+#    #+#             */
-/*   Updated: 2021/11/13 02:26:08 by hmokhtar         ###   ########.fr       */
+/*   Updated: 2021/11/18 04:04:07 by hmokhtar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	srclen;
 	size_t	i;
 
-	if (!src)
-		return (0);
 	srclen = ft_strlen(src);
 	i = 0;
 	if (size != 0)
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: hmokhtar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 01:51:34 by hmokhtar          #+#    #+#             */
-/*   Updated: 2021/11/14 21:53:55 by hmokhtar         ###   ########.fr       */
+/*   Updated: 2021/11/18 04:03:43 by hmokhtar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,13 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t len)
 	size_t	j;
 
 	i = 0;
-	if (len == 0 || s1 == s2)
+	if (*s2 == '\0')
 		return ((char *)s1);
 	while (s1[i] != '\0' && i < len)
 	{
 		j = 0;
 		while (s2[j] && s1[i + j] == s2[j] && i + j < len)
 			j++;
-		if (!len && !s2[j])
-			return (NULL);
 		if (s2[j] == '\0')
 			return ((char *)s1 + i);
 		i++;
