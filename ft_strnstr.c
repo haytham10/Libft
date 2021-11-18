a683e4f585bcf801feda939e8583f39be661d2c2
haytham10 <cnarabi@gmail.com>
2021-11-18 03:51:25 +0100

    03511811

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -12,30 +12,24 @@
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *s1, const char *s2, size_t n)
+char	*ft_strnstr(const char *s1, const char *s2, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	size_t	len;
 
-	if (!s1 || !s2)
-		return (NULL);
-	if (*s2 == '\0')
+	i = 0;
+	if (len == 0 || s1 == s2)
 		return ((char *)s1);
-	i = -1;
-	while (s1[++i] && n)
+	while (s1[i] != '\0' && i < len)
 	{
 		j = 0;
-		len = n;
-		while (s2[j] && s2[j] == s1[i + j] && len--)
-		{
+		while (s2[j] && s1[i + j] == s2[j] && i + j < len)
 			j++;
-			if (!s2[j])
-				return ((char *)(s1 + i));
-			if (!len && !s2[j])
-				return (NULL);
-		}
-		n--;
+		if (!len && !s2[j])
+			return (NULL);
+		if (s2[j] == '\0')
+			return ((char *)s1 + i);
+		i++;
 	}
 	return (NULL);
 }
