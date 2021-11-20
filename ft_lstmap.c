729ec5f8c6c221460fa0ba61ecde28e668a0e5a4
haytham10 <cnarabi@gmail.com>
2021-11-20 03:40:54 +0100

    03402011

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hmokhtar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: hmokhtar <hmokhtar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 01:06:59 by hmokhtar          #+#    #+#             */
-/*   Updated: 2021/11/19 20:04:22 by hmokhtar         ###   ########.fr       */
+/*   Updated: 2021/11/20 03:40:42 by hmokhtar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,22 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*new;
+	t_list	*first;
+
+	if (!lst || !f)
+		return (NULL);
+	first = 0;
+	while (lst)
+	{
+		new = ft_lstnew((*f)(lst->content));
+		if (!new)
+		{
+			ft_lstclear(&first, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&first, new);
+		lst = lst->next;
+	}
+	return (first);
 }
