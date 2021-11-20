36d6832642bef85041be0de42990e1b3faff5059
haytham10 <cnarabi@gmail.com>
2021-11-20 04:39:41 +0100

    04382011

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,12 +6,12 @@
 /*   By: hmokhtar <hmokhtar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 00:51:04 by hmokhtar          #+#    #+#             */
-/*   Updated: 2021/11/20 04:35:41 by hmokhtar         ###   ########.fr       */
+/*   Updated: 2021/11/20 04:39:23 by hmokhtar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	if (!del || !lst)
